a45d434c41f9e74e26f3e5fd34bba20784db294a
ryuto <n44n7zb3mvangjzykv1m@docomo.ne.jp>
2023-09-06 06:26:32 +0000

    開発環境のViewを一部変更

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -15,12 +15,12 @@
                                     @endif
                                 </div>
                                 <div class="title p-2">
-                                    <h1>{{ Str::limit($headline->title, 70) }}</h1>
+                                    <h1>ニュースタイトル:{{ Str::limit($headline->title, 70) }}</h1>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-6">
-                            <p class="body mx-auto">{{ Str::limit($headline->body, 650) }}</p>
+                            <p class="body mx-auto">ニュース本文:{{ Str::limit($headline->body, 650) }}</p>
                         </div>
                     </div>
                 </div>
@@ -37,10 +37,10 @@
                                     {{ $post->updated_at->format('Y年m月d日') }}
                                 </div>
                                 <div class="title">
-                                    {{ Str::limit($post->title, 150) }}
+                                    ニュースタイトル:{{ Str::limit($post->title, 150) }}
                                 </div>
                                 <div class="body mt-3">
-                                    {{ Str::limit($post->body, 1500) }}
+                                    ニュース本文:{{ Str::limit($post->body, 1500) }}
                                 </div>
                             </div>
                             <div class="image col-md-6 text-right mt-4">
--- routes/web.php
+++ routes/web.php
@@ -60,7 +60,7 @@ use App\Http\Controllers\NewsController as PublicNewsController;
 Route::get('/', [PublicNewsController::class, 'index'])->name('news.index');
 
 use App\Http\Controllers\ProfileController as PublicProfileController;
-Route::get('/', [PublicProfileController::class, 'index'])->name('profile.index');
+Route::get('/profile', [PublicProfileController::class, 'index'])->name('profile.index');
 
 Auth::routes();
 
